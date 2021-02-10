4c5afe781e5ff9fd273eb7ccdfeae1987cbf4109
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-02-10 19:02:07 +0100

    Create fr.php
    for elgg v 1.0.2

---
+++ integrations/elgg/freichat/languages/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'freichat:settings:token' => 'Jeton',
+  'freichat:settings:token:help' => 'Un jeton utilisé pour une communication sécurisée. Veuillez ne pas modifier ce point.',
+  'freichat:settings:friends' => 'Voir seulement les contacts ?',
+  'freichat:settings:friends:help' => 'Dans ce cas, seuls les utilisateurs qui sont en contact seront listés.',
+);
